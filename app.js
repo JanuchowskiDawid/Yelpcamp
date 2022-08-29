d15bd0c39f98ec0b08662a04b142efb57a37f6f6
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-29 17:52:00 +0200

    Finished basic CRUD, delete feature

--- app.js
+++ app.js
@@ -55,6 +55,12 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
     res.render('campgrounds/edit', { campground });
 })
 
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect(`/campgrounds`);
+})
+
 app.listen(3000, () => {
     console.log('Serving on port 3000');
 })
