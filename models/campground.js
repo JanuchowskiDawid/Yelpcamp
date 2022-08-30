ae43343aacb59ad1b15217acba9ebc0f33d38f6f
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-30 15:37:03 +0200

    Made stylish with bootstrap and ejs-mate

--- models/campground.js
+++ models/campground.js
@@ -3,8 +3,9 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
-    descriptnion: String,
+    image: String,
+    price: Number,
+    description: String,
     location: String
 })
 
--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const randomPrice = Math.floor(Math.random() * 50) + 10;
         const newCampground = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/9046579',
+            description: 'csawadasfaffsaafasfsaffas',
+            price: randomPrice
         })
         await newCampground.save();
     }
